6cd6dc1ff3d3e292540c90507386c3b8dd3e2ad3
Brian Lawi Ochiel <lawioochiel@gmail.com>
2024-01-17 18:32:49 +0300

    completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,35 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+
+    sequence = []
+
+    if length > 0:
+        sequence.append(0)
+
+    if length >= 2:
+        sequence.append(1)
+
+        for i in range(2,length):
+            sequence.append(sequence[-1] + sequence[-2])
+
+    print(sequence)
+
+# def print_fibonacci(length):
+
+    
+# 	sequence = []
+	
+    
+# 	if length > 0:
+# 		sequence.append(0)
+		
+    
+# 	if length >= 2:
+# 		sequence.append(1)
+		
+        
+# 		for i in range(2, length):
+# 			sequence.append(sequence[-1] + sequence[-2])
+
+# 	print(sequence)
